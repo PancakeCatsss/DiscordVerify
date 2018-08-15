0c3b5825c21e44f2fee9ce3fcb1a1481bac63607
PancakeCatsss <Aaron_jennings2003@hotmail.co.uk>
2018-08-15 19:30:16 +0100

    Added a check for their rank

--- src/me/bobcatsss/discordverify/commands/VerifyCommand.java
+++ src/me/bobcatsss/discordverify/commands/VerifyCommand.java
@@ -7,6 +7,8 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import me.bobcatsss.discordverify.utils.Utils;
+import ru.tehkode.permissions.PermissionUser;
+import ru.tehkode.permissions.bukkit.PermissionsEx;
 import simple.brainsynder.nms.ITellraw;
 import simple.brainsynder.utils.Reflection;
 
@@ -19,11 +21,16 @@ public class VerifyCommand implements CommandExecutor {
 			return true;
 		}
 		Player player = (Player) sender;
+		PermissionUser user = PermissionsEx.getUser(player);
+		if(user.getGroups().length == 1) {
+			player.sendMessage(Utils.c("&f[&eDiscordVerify&f] &cIn order to join our discord server you must be a higher rank than Builder&f. Please use &e/ar check &fto see when you rank up&f."));
+			return true;
+		}
 		String code = Utils.randomID("1234567890ABCDEFG", 10);
-                ITellraw msg = Reflection.getTellraw("§f[§eDiscordVerify§f] §eYour code is§f: ");
+                ITellraw msg = Reflection.getTellraw(Utils.c("&f[&eDiscordVerify&f] &eYour code is&f: "));
                 msg.color (ChatColor.GREEN); 
                 msg.then(ChatColor.UNDERLINE + code);
-                msg.tooltip("§aClick here to copy");
+                msg.tooltip(Utils.c("&aClick here to copy"));
                 msg.suggest(code);
                 msg.send(player);
 		return true;
